1e416e64835a3f4ca86323c9976c2ad2147266bf
kingpowerobot <kingpowerad095@gmail.com>
2016-03-26 14:46:00 +0430

    Delete remam.lua

--- plugins/remam.lua
+++
@@ -1,9 +0,0 @@
-do local function run(msg, matches) if not is_admin(msg) then return nil end
-
-if msg.to.type == 'chat'then
-
-if matches[1] == "حذف ادمین" then Channel = 'chat#'..msg.to.id user = 'user#'..matches[2] chat_rem_admin(Chat, user, ok_cb, true) return  user..'\nاز ادمینی بات خارج شد end
-
-end end  return { description = "", usage = {}, patterns = { "^(حذف ادمین) (%d+)$" }, run = run }
-
-end

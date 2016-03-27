2cc9fe8fa1c97a08515f78458496ad7c06df7b80
kingpowerobot <kingpowerad095@gmail.com>
2016-03-28 00:27:57 +0430

    Update fosh.lua

--- plugins/fosh.lua
+++ plugins/fosh.lua
@@ -1,14 +1,26 @@
-       do
-          function run(msg, matches)
-       local bye_name = msg.action.user.first_name
-       return '\nمادر قهوه ننه کیر پرست پول ننتو ندادم پورو شدی به ننت یه تراول صدی دادم داره هر روز به هم می ده برو ننتو جمع کن' ..bye_name 
-       end
+
+   
+local function run(msg, matches)
+  local text = matches[1]
+  local b = 1
+
+  while b ~= 0 do
+    text = text:trim()
+    text,b = text:gsub('^!+','')
+  end
+  return 'مادر قهوه ننه کیر پرست پول ننتو ندادم پورو شدی به ننت یه تراول صدی دادم داره هر روز به هم می ده برو ننتو جمع کن' ..text
+end
+
 return {
-   description = "Welcoming Message",
-   usage = "send message to new member",
-   patterns = {
-      "^[!/#]fosh (.*)$",
-      "^فحش (.*)$"
-   },
-   run = run
+  description = "fohsh",
+  usage = {
+  "فحش[who]: فحش به دیگران",
+  "fosh [who]: tpa به یگران",
+  },
+  patterns = {
+    "^فحش +(.+)$",
+	"^[/!#]fosh +(.+)$",
+	"^fosh +(.+)$"
+  }, 
+  run = run 
 }

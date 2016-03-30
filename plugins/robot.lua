dfef3cc191d60a1ad873c54254c8e300e5308e9b
botmohammad12344888 <alihoshmand43@gmail.com>
2016-03-30 21:51:16 +0430

    Update robot.lua

--- plugins/robot.lua
+++ plugins/robot.lua
@@ -10,22 +10,22 @@ local function is_channel_disabled( receiver )
   return _config.disabled_channels[receiver]
 end
 
-local function enable_channel(receiver, to_id)
+local function enable_channel(receiver)
 	if not _config.disabled_channels then
 		_config.disabled_channels = {}
 	end
 
 	if _config.disabled_channels[receiver] == nil then
-		return  'ربات روشن است'
+		return 'Channel isn\'t disabled'
 	end
 	
 	_config.disabled_channels[receiver] = false
 
 	save_config()
-	return 'ربات روشن است'
+	return "Channel re-enabled"
 end
 
-local function disable_channel(receiver, to_id)
+local function disable_channel( receiver )
 	if not _config.disabled_channels then
 		_config.disabled_channels = {}
 	end
@@ -33,45 +33,49 @@ local function disable_channel(receiver, to_id)
 	_config.disabled_channels[receiver] = true
 
 	save_config()
-	return  'ربات خاموشه'
+	return "Bot Turned Off"
 end
 
 local function pre_process(msg)
 	local receiver = get_receiver(msg)
 	
-	-- If sender is sudo then re-enable the channel
-	if is_sudo(msg) then
-	  if msg.text == "ربات روشن" then
-	    enable_channel(receiver, msg.to.id)
+	-- If sender is moderator then re-enable the channel
+	--if is_sudo(msg) then
+	if is_momod(msg) then
+	  if msg.text == "bot on" then
+	    enable_channel(receiver)
 	  end
 	end
 
   if is_channel_disabled(receiver) then
   	msg.text = ""
   end
+
 	return msg
 end
 
 local function run(msg, matches)
-	if permissions(msg.from.id, msg.to.id, "ربات") then
-		local receiver = get_receiver(msg)
-		-- Enable a channel
-		if matches[1] == 'روشن' then
-			return enable_channel(receiver, msg.to.id)
-		end
-		-- Disable a channel
-		if matches[1] == 'خاموش' then
-			return disable_channel(receiver, msg.to.id)
-		end
-	else
-		return 'فقط سودو'
+	local receiver = get_receiver(msg)
+	-- Enable a channel
+	if matches[1] == 'on' then
+		return enable_channel(receiver)
+	end
+	-- Disable a channel
+	if matches[1] == 'off' then
+		return disable_channel(receiver)
 	end
 end
 
 return {
+	description = "Plugin to manage Bot.", 
+	usage = {
+		"Bot on: enable BOT In a Group",
+		"Bot off: disable Bot In a Group" },
 	patterns = {
-		"^ربات? (روشن)",
-		"^ربات? (خاموش)" }, 
+		"^[Bb]ot? (on)",
+		"^[Bb]ot? (off)" }, 
 	run = run,
+	privileged = true,
+	--moderated = true,
 	pre_process = pre_process
 }

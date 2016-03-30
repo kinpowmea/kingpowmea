ed49512440832b128005d74a4efa4e77134e47c1
botmohammad12344888 <alihoshmand43@gmail.com>
2016-03-30 23:08:58 +0430

    Update helpen.lua

--- plugins/helpen.lua
+++ plugins/helpen.lua
@@ -3,7 +3,20 @@ function run(msg, matches)
   return [[ 
 
   Commands list :
-
+for owner group:
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+〰〰〰〰〰〰
+!promote [username]
+Promote someone
+〰〰〰〰〰〰
+!demote [username]
+Demote someone
+〰〰〰〰〰〰
+!clean [modlist|rules|about]
+Will clear [modlist|rules|about] 
+〰〰〰〰〰〰
+for moderators group:
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
 !kick [username|id]
 You can also by reply
 〰〰〰〰〰〰
@@ -22,32 +35,11 @@ Members list
 !modlist
 Moderators list
 〰〰〰〰〰〰
-!promote [username]
-Promote someone
-〰〰〰〰〰〰
-!demote [username]
-Demote someone
-〰〰〰〰〰〰
-!kickme
-Will kick user
-〰〰〰〰〰〰
-!about
-Group description
-〰〰〰〰〰〰
 !setphoto
 locks group photo
 〰〰〰〰〰〰
 !setname [name]
 Set group name
-〰〰〰〰〰〰
-!rules
-Group rules
-〰〰〰〰〰〰
-!id
- group id & user id
-〰〰〰〰〰〰
-!help
-This help persian text
 〰〰〰〰〰〰
 !lock [member|name|bots|leave|arabic|tag|ads] 
 Locks [member|name|bots|leaveing|arabic|tag|ads] 
@@ -70,17 +62,8 @@ create/revoke your group link
 !link
 returns group link
 〰〰〰〰〰〰
-!owner
-returns group owner id
-〰〰〰〰〰〰
-!setowner [id]
-Will set id as owner
-〰〰〰〰〰〰
 !setflood [value]
 Set [value] as flood sensitivity
-〰〰〰〰〰〰
-!stats
-Simple message statistics
 〰〰〰〰〰〰
 !save [value] <text>
 Save <text> as [value]
@@ -88,53 +71,22 @@ Save <text> as [value]
 !get [value]
 Returns text of [value]
 〰〰〰〰〰〰
-!clean [modlist|rules|about]
-Will clear [modlist|rules|about] 
-〰〰〰〰〰〰
-!info 
-send you a user stats 
-worked by reply
-〰〰〰〰〰〰
-!sticker [warn|kick|ok]
-warn : warning send sticker
-kick : send sticker=kick
-ok : send sticker open
-〰〰〰〰〰〰
 !tagall [text]
 tag users && send your message
 〰〰〰〰〰〰
-!all
-see all about group
-〰〰〰〰〰〰
-!block (user-id)
-!unblock (user-id)
-block - unblock users (sudo only)
-〰〰〰〰〰〰
 !kickinactive
 kick inactive users from Group
-〰〰〰〰〰〰
-!pv [user-id] [text]
-send text to user-id (sudo only)
-〰〰〰〰〰〰
-!linkpv
-send link to your pv ( bot reported)
-〰〰〰〰〰〰
-!banlist
-group ban list
 〰〰〰〰〰〰
 !welcome [group|pm|disable]
 set welcome to group
 set welcome to pm (pv)
 set welcome disable
 〰〰〰〰〰〰
-!calc (s.th)
-calculation math
+!linkpv
+send link to your pv ( bot reported)
 〰〰〰〰〰〰
-!echo (s.th)
-bot say s.th that you typed
-〰〰〰〰〰〰
-!feedback (s.th)
-send a feedback to admin
+!banlist
+group ban list
 〰〰〰〰〰〰
 !fliter + word
 if s.one say it bot kick him
@@ -145,8 +97,51 @@ remove a word from filterlist
 !filterlist
 you can see a filter list
 〰〰〰〰〰〰
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+for members:
+!rules
+Group rules
+〰〰〰〰〰〰
+!kickme
+Will kick user
+〰〰〰〰〰〰
+!about
+Group description
+〰〰〰〰〰〰
+!id
+ group id & user id
+〰〰〰〰〰〰
+!help
+This help persian text
+〰〰〰〰〰〰
+!owner
+returns group owner id
+〰〰〰〰〰〰
+!stats
+Simple message statistics
+〰〰〰〰〰〰
+!info 
+send you a user stats 
+worked by reply
+〰〰〰〰〰〰
+!all
+see all about group
+〰〰〰〰〰〰
 !info
 information about you and members
+〰〰〰〰〰〰
+!me
+you can see who are you in group?[sudo,admin,owner,moderatior,member]
+part fun bot:
+⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+!calc (s.th)
+calculation math
+〰〰〰〰〰〰
+!echo (s.th)
+bot say s.th that you typed
+〰〰〰〰〰〰
+!feedback (s.th)
+send a feedback to admin
 〰〰〰〰〰〰
 !tosticker
 convert a photo to sticker
@@ -163,18 +158,8 @@ search something of you entered in google
 !img [s.th]
 search something of you entered in image google
 〰〰〰〰〰〰
-!quran
-show the list of the qurans
-!sura (number)
-or
-!write (number)
-read the number of quran 
-〰〰〰〰〰〰
 !map [area]
 you can see that areas location
-〰〰〰〰〰〰
-!me
-you can see who are you in group?[sudo,admin,owner,moderatior,member]
 〰〰〰〰〰〰
 !t2i [text]
 convert a text to image
@@ -185,6 +170,18 @@ convert a text to a voice
 !web [url]
 take a webshot from a site(your site should to be start https or http
 〰〰〰〰〰〰
+!aparat (text)
+search in the aparat.com
+〰〰〰〰〰〰
+!praytime (city)
+show that praytime of city
+〰〰〰〰〰〰
+!translate (text) or (laung,laung,text)
+translate
+〰〰〰〰〰〰
+!weather (city)
+show the weather of that city
+〰〰〰〰〰〰
 **U can use "/" & "!"
 〰〰〰〰〰〰
 *Only owner & mods can add bots to group
@@ -192,7 +189,11 @@ take a webshot from a site(your site should to be start https or http
 *Only moderators & owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about,settings,filterword commands
 〰〰〰〰〰〰
 *Only owner can use res,setowner,promote,demote,log commands
-
+admins:
+@kingpower_admin_1
+@AH2002
+channel:
+@kingpowerch
   ]]
 end
 
